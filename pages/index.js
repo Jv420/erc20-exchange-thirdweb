d2bd2954ea045047ac1c2c24342b41a1c7014b78
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2024-06-10 10:41:16 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Exchange ATH tokens</title>
+        <title>Exchange VloeiCoin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="home__container">
